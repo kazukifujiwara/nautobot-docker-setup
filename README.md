5fb0019f0c3c7526618b64af06197fc7a6ca7b9e
Kazuki Fujiwara <kazuki@Kazuki-MacBook-Pro.local>
2022-05-16 00:09:07 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # nautobot-docker-setup
+
+This is a script to start nautobot on an instance with no configuration.
+
+## Tested Environment
+
+### Ubuntu
+
+```
+DISTRIB_ID=Ubuntu
+DISTRIB_RELEASE=22.04
+DISTRIB_CODENAME=jammy
+DISTRIB_DESCRIPTION="Ubuntu 22.04 LTS"
+```
+
+## Reference
+
+* [nautobot-docker-compose](https://github.com/nautobot/nautobot-docker-compose)
+
---
+++ ubuntu-nautobot-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update && sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
+
+git clone https://github.com/nautobot/nautobot-docker-compose.git
+cd nautobot-docker-compose
+cp local.env.example local.env
+
+# If you don't want to create "admin" user with default setting, comment out the following.
+cat local.env | sed s/NAUTOBOT_CREATE_SUPERUSER=false/NAUTOBOT_CREATE_SUPERUSER=true/ > local.env
+
+# The following should be changed for proper security!
+# vi local.env
+
+chmod 0600 local.env
+
+sudo docker-compose up -d
